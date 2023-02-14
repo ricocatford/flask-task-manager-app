3d9617240bb4320b77ed2aab87ea00a265fbdfcb
ricocatford <s7funoob@gmail.com>
2023-02-14 00:00:22 +0000

    Bind data to task accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
-});
-
-$(document).ready(function(){
     $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
 });
